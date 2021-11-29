675ae69d0034a26e8f2edf288f8ba80823ae7700
Tim Ebert <tim.ebert@sap.com>
2021-11-29 23:21:55 +0100

    Deploy to build cluster as well

--- config/prow/deploy.sh
+++ config/prow/deploy.sh
@@ -13,6 +13,18 @@ color-green() { # Green
   echo -e "\x1B[1;32m${@}\x1B[0m"
 }
 
+color-step() { # Yellow
+  echo -e "\x1B[1;33m${@}\x1B[0m"
+}
+
+color-context() { # Bold blue
+  echo -e "\x1B[1;34m${@}\x1B[0m"
+}
+
+color-missing() { # Yellow
+  echo -e "\x1B[1;33m${@}\x1B[0m"
+}
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 prow_components=(
@@ -52,17 +64,53 @@ prow_components=(
 "tide_service.yaml"
 )
 
+prow_components_build=(
+"test-pods_namespace.yaml"
+"cpu-limit-range_limitrange.yaml"
+"mem-limit-range_limitrange.yaml"
+)
+
 if ! [ -x "$(command -v "kubectl")" ]; then
   echo "ERROR: kubectl is not present. Exiting..."
   exit 1
 fi
 
-echo "$(color-green Deploying Prow...)"
+ensure-context() {
+  local context=$1
+  echo -n " $(color-context "$context")"
+  kubectl config get-contexts "$context" &> /dev/null && return 0
+  echo ": $(color-missing MISSING), stopping..."
+  return 1
+}
 
+current_context=$(kubectl config current-context 2>/dev/null || true)
+restore-context() {
+  if [[ -n "$current_context" ]]; then
+    kubectl config set-context "$current_context"
+  fi
+}
+trap restore-context EXIT
+
+echo -n "$(color-step "Ensuring contexts exist"):"
+ensure-context gardener-prow-trusted
+ensure-context gardener-prow-build
+echo " $(color-green done)"
+
+echo "$(color-step "Deploying prow components to gardener-prow-trusted cluster...")"
+kubectl config set-context gardener-prow-trusted
 # use server-side apply for CRD, otherwise annotation will be too long
 kubectl apply --server-side=true -f "$SCRIPT_DIR/cluster/prowjob_customresourcedefinition.yaml"
 
 for c in "${prow_components[@]}"; do
   kubectl apply -f "$SCRIPT_DIR/cluster/$c"
 done
+echo "$(color-green done)"
+
+echo "$(color-step "Deploying prow components to gardener-prow-build cluster...")"
+kubectl config set-context gardener-prow-build
+for c in "${prow_components_build[@]}"; do
+  kubectl apply -f "$SCRIPT_DIR/cluster/$c"
+done
+echo "$(color-green done)"
+
 echo "$(color-green SUCCESS)"
