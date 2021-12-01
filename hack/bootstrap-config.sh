ff1260ed5f06ac90d05f79ee97a4cd4cadafe772
Gardener Prow Robot <95282030+gardener-ci-robot@users.noreply.github.com>
2021-12-01 11:03:37 +0100

    Bumping Prow (#5)
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/0afc0f8086...aa97daa0b4 | 2021&#x2011;09&#x2011;28&nbsp;&#x2192;&nbsp;2021&#x2011;12&#x2011;01 | clonerefs, crier, deck, entrypoint, ghproxy, hook, horologium, initupload, label_sync, needs-rebase, prow-controller-manager, sidecar, sinker, status-reconciler, tide
    https://github.com/kubernetes/test-infra/compare/48cb2fc883...aa97daa0b4 | 2021&#x2011;11&#x2011;26&nbsp;&#x2192;&nbsp;2021&#x2011;12&#x2011;01 | checkconfig, config-bootstrapper, generic-autobumper, mkpj

--- hack/bootstrap-config.sh
+++ hack/bootstrap-config.sh
@@ -27,7 +27,7 @@ fi
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
   -v "$kubeconfig":/etc/kubeconfig \
-  gcr.io/k8s-prow/config-bootstrapper:v20211126-48cb2fc883 \
+  gcr.io/k8s-prow/config-bootstrapper:v20211201-aa97daa0b4 \
   --kubeconfig=/etc/kubeconfig \
   --source-path=.  \
   --config-path=config/prow/config.yaml \
