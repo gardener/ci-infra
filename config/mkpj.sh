42feae2e59cde2230e2eecf3fe2d42519ff22279
Gardener Prow Robot <95282030+gardener-ci-robot@users.noreply.github.com>
2022-01-24 15:36:43 +0100

    Bumping Prow (#51)
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/49cbff1954...30b07d16e7 | 2022&#x2011;01&#x2011;18&nbsp;&#x2192;&nbsp;2022&#x2011;01&#x2011;21 | generic-autobumper
    https://github.com/kubernetes/test-infra/compare/bb77a9cb93...30b07d16e7 | 2022&#x2011;01&#x2011;21&nbsp;&#x2192;&nbsp;2022&#x2011;01&#x2011;21 | branchprotector, checkconfig, clonerefs, config-bootstrapper, crier, deck, entrypoint, generic-autobumper, ghproxy, hook, horologium, initupload, label_sync, mkpj, needs-rebase, prow-controller-manager, sidecar, sinker, status-reconciler, tide

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -26,7 +26,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/mkpj:v20220121-bb77a9cb93 \
+  gcr.io/k8s-prow/mkpj:v20220121-30b07d16e7 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
