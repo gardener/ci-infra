4d1fce03ffb09be7964af30773f6b2d44853f3f2
Gardener Prow Robot <95282030+gardener-ci-robot@users.noreply.github.com>
2022-02-14 15:37:14 +0100

    Bumping Prow and Prow - ci-infra (#84)
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/180c042a80...1fa5b1ec33 | 2021&#x2011;12&#x2011;20&nbsp;&#x2192;&nbsp;2022&#x2011;02&#x2011;14 | commenter
    https://github.com/kubernetes/test-infra/compare/08f046e294...1fa5b1ec33 | 2022&#x2011;02&#x2011;10&nbsp;&#x2192;&nbsp;2022&#x2011;02&#x2011;14 | branchprotector, checkconfig, clonerefs, commenter, config-bootstrapper, crier, deck, entrypoint, generic-autobumper, ghproxy, hook, horologium, initupload, label_sync, mkpj, needs-rebase, prow-controller-manager, sidecar, sinker, status-reconciler, tide
    
    No ghcr.io/gardener/ci-infra/ changes.

--- hack/bootstrap-config.sh
+++ hack/bootstrap-config.sh
@@ -27,7 +27,7 @@ fi
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
   -v "$kubeconfig":/etc/kubeconfig \
-  gcr.io/k8s-prow/config-bootstrapper:v20220210-08f046e294 \
+  gcr.io/k8s-prow/config-bootstrapper:v20220214-1fa5b1ec33 \
   --kubeconfig=/etc/kubeconfig \
   --source-path=.  \
   --config-path=config/prow/config.yaml \
