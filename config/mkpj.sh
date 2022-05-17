55b193c9e0cd30e31e5dd77fad6b52efa0691ba2
Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>
2022-05-17 16:33:23 +0200

    Revert "Bumping Prow and Prow - ci-infra (#250)"
    This reverts commit 3a025918620440e535731af849b6fc130b0df04a.

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -26,7 +26,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/mkpj:v20220516-bc723c0e51 \
+  gcr.io/k8s-prow/mkpj:v20220513-69ff551a87 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
