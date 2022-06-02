47315baf7ab5ff5c8448ab38c0f8b5c01d70b838
oliver-goetz <o.goetz@sap.com>
2022-06-02 12:20:04 +0200

    Create a script for checking testgrid config locally

---
+++ hack/check-testgrid-config.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cd "$(git rev-parse --show-toplevel)"
+
+docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
+  gcr.io/k8s-prow/transfigure:v20220523-6e12e42c76 \
+  test \
+  config/prow/config.yaml \
+  config/jobs \
+  config/prow/gardener-testgrid.yaml \
+  gardener \
+  kubernetes-test-infra
