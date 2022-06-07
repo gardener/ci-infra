a6dbf1e5c75993a73001051a5cfc681225251375
Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>
2022-06-07 16:09:29 +0200

    Deploy prow components using kustomize (#264)
    * Deploy prow components using kustomize
    
    * Use kustomize overlay for shared settings

--- config/prow/deploy.sh
+++ config/prow/deploy.sh
@@ -40,59 +40,6 @@ color-missing() { # Yellow
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-prow_components=(
-"prow_namespace.yaml"
-"test-pods_namespace.yaml"
-"cherrypicker_deployment.yaml"
-"cherrypicker_service.yaml"
-"builder_serviceaccount.yaml"
-"cla_assistant_deployment.yaml"
-"cla_assistant_service.yaml"
-"cpu-limit-range_limitrange.yaml"
-"crier_deployment.yaml"
-"crier_rbac.yaml"
-"crier_service.yaml"
-"deck_deployment.yaml"
-"deck_rbac.yaml"
-"deck_service.yaml"
-"gce-ssd_storageclass.yaml"
-"gcsweb_namespace.yaml"
-"gcsweb_deployment.yaml"
-"gcsweb_service.yaml"
-"gcsweb_ingress.yaml"
-"ghproxy_deployment.yaml"
-"ghproxy_pvc.yaml"
-"ghproxy_service.yaml"
-"hook_deployment.yaml"
-"hook_rbac.yaml"
-"hook_service.yaml"
-"horologium_deployment.yaml"
-"horologium_rbac.yaml"
-"horologium_service.yaml"
-"prow_ingress.yaml"
-"mem-limit-range_limitrange.yaml"
-"needs-rebase_deployment.yaml"
-"needs-rebase_service.yaml"
-"prow_controller_manager_deployment.yaml"
-"prow_controller_manager_rbac.yaml"
-"prow_controller_manager_service.yaml"
-"sinker_deployment.yaml"
-"sinker_rbac.yaml"
-"sinker_service.yaml"
-"statusreconciler_deployment.yaml"
-"statusreconciler_rbac.yaml"
-"tide_deployment.yaml"
-"tide_rbac.yaml"
-"tide_service.yaml"
-)
-
-prow_components_build=(
-"test-pods_namespace.yaml"
-"cpu-limit-range_limitrange.yaml"
-"mem-limit-range_limitrange.yaml"
-"gce-ssd_storageclass.yaml"
-)
-
 if ! [ -x "$(command -v "kubectl")" ]; then
   echo "ERROR: kubectl is not present. Exiting..."
   exit 1
@@ -124,19 +71,12 @@ echo " $(color-green done)"
 
 echo "$(color-step "Deploying prow components to gardener-prow-trusted cluster...")"
 kubectl config use-context gardener-prow-trusted
-# use server-side apply for CRD, otherwise annotation will be too long
-kubectl apply --server-side=true -f "$SCRIPT_DIR/cluster/prowjob_customresourcedefinition.yaml"
-
-for c in "${prow_components[@]}"; do
-  kubectl apply --server-side=true -f "$SCRIPT_DIR/cluster/$c"
-done
+kubectl apply --server-side=true -k "$SCRIPT_DIR/cluster"
 echo "$(color-green done)"
 
 echo "$(color-step "Deploying prow components to gardener-prow-build cluster...")"
 kubectl config use-context gardener-prow-build
-for c in "${prow_components_build[@]}"; do
-  kubectl apply --server-side=true -f "$SCRIPT_DIR/cluster/$c"
-done
+kubectl apply --server-side=true -k "$SCRIPT_DIR/cluster/base"
 echo "$(color-green done)"
 
 echo "$(color-step "Deploying ingress-nginx components to gardener-prow-trusted cluster...")"
