f6dd73021c690eaf0e1cd48ccc2c63d6534674e7
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2022-07-12 01:30:54 +0000

    Bumping Prow and Prow - ci-infra
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes/test-infra/compare/6c9b2eb5c9...8e52bef68a (2022&#x2011;07&#x2011;07 → 2022&#x2011;07&#x2011;12)
    
    No eu.gcr.io/gardener-project/ci-infra/ changes.

--- hack/check-config.sh
+++ hack/check-config.sh
@@ -20,7 +20,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/checkconfig:v20220707-6c9b2eb5c9 \
+  gcr.io/k8s-prow/checkconfig:v20220712-8e52bef68a \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   --plugin-config=config/prow/plugins.yaml \
--- hack/check-testgrid-config.sh
+++ hack/check-testgrid-config.sh
@@ -20,7 +20,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/configurator:v20220707-6c9b2eb5c9 \
+  gcr.io/k8s-prow/configurator:v20220712-8e52bef68a \
   --yaml=config/testgrids/config.yaml \
   --default=config/testgrids/default.yaml \
   --prow-config=config/prow/config.yaml \
