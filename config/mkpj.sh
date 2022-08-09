5ddcf773fd3ecdb884245811d19c1b7a182db9cb
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2022-08-09 03:30:45 +0000

    Bumping Prow and Prow - ci-infra
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes/test-infra/compare/565e38c0a7...e2d16d6fdc (2022&#x2011;08&#x2011;05 → 2022&#x2011;08&#x2011;09)
    
    No eu.gcr.io/gardener-project/ci-infra/ changes.

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -26,7 +26,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/mkpj:v20220805-565e38c0a7 \
+  gcr.io/k8s-prow/mkpj:v20220809-e2d16d6fdc \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
