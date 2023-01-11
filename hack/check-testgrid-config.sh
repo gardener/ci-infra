b39a84609f56e6edbc42ee1bd286af230350fcd2
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2023-01-11 23:30:49 +0000

    Bumping Prow and Prow - ci-infra
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes/test-infra/compare/62b7489b86...f89d0e1985 (2023&#x2011;01&#x2011;10 → 2023&#x2011;01&#x2011;11)
    
    No eu.gcr.io/gardener-project/ci-infra/ changes.

--- hack/check-testgrid-config.sh
+++ hack/check-testgrid-config.sh
@@ -20,7 +20,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/configurator:v20230110-62b7489b86 \
+  gcr.io/k8s-prow/configurator:v20230111-f89d0e1985 \
   --yaml=config/testgrids/config.yaml \
   --default=config/testgrids/default.yaml \
   --prow-config=config/prow/config.yaml \
