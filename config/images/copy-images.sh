6295de571d5cb8bfc33c9ff50e5db0fd99ac5db5
Robin Schneider <robin.schneider@stackit.de>
2023-03-21 09:56:57 +0100

    Added prow job to copy images

---
+++ config/images/copy-images.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+filepath="$1"
+if [ -z "$filepath" ] ; then
+  echo "Please specify a path to the images.yaml configuration file as the first argument."
+  exit 1
+fi
+
+# Loop over the images in the YAML
+for source in $(yq '.images[].source' "$filepath"); do
+    image=$(yq '.images[] | select(.source == "'"$source"'")' "$filepath")
+    destination=$(echo "$image" | yq '.destination')
+    # Loop over the tags for the current image
+    for tag in $(echo "$image" | yq '.tags[]'); do
+        # Copy the container image for the current tag using crane
+        crane copy "$source:$tag" "$destination:$tag"
+    done
+done
