aec5fe5c4b899ac71892ca5280f77f980bd7dc3f
Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>
2023-05-11 14:11:30 +0200

    Update to `kube-prometheus@v0.12.0` (#717)

--- config/prow/cluster/monitoring/base/update.sh
+++ config/prow/cluster/monitoring/base/update.sh
@@ -20,7 +20,7 @@ set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $script_dir
 
-kube_prometheus_version="v0.11.0"
+kube_prometheus_version="v0.12.0"
 echo "> Fetching kube-prometheus@$kube_prometheus_version"
 
 tmp_dir=$(mktemp -d)
