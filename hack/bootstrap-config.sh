fad9bd56ec744a3f8dfee2aac88e009ec55a4c10
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2023-07-27 10:37:40 +0200

    Bumping Prow and Prow - ci-infra (#801)
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes/test-infra/compare/371721ec95...ff70aa5b9e (2023&#x2011;07&#x2011;25 → 2023&#x2011;07&#x2011;27)
    
    No eu.gcr.io/gardener-project/ci-infra/ changes.

--- hack/bootstrap-config.sh
+++ hack/bootstrap-config.sh
@@ -77,7 +77,7 @@ cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
   -v "$temp_kubeconfig":/etc/kubeconfig \
-  gcr.io/k8s-prow/config-bootstrapper:v20230725-371721ec95 \
+  gcr.io/k8s-prow/config-bootstrapper:v20230727-ff70aa5b9e \
   --kubeconfig=/etc/kubeconfig \
   --source-path=.  \
   --config-path=config/prow/config.yaml \
--- hack/check-config.sh
+++ hack/check-config.sh
@@ -20,7 +20,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/checkconfig:v20230725-371721ec95 \
+  gcr.io/k8s-prow/checkconfig:v20230727-ff70aa5b9e \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   --plugin-config=config/prow/plugins.yaml \
