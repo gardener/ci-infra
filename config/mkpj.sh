9cd3f8c7e1913650617cc142f2ff519c7145f1c4
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2023-08-10 10:36:25 +0200

    Bumping Prow and Prow - ci-infra (#834)
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes/test-infra/compare/2811823aac...b1472a35ec (2023&#x2011;08&#x2011;08 → 2023&#x2011;08&#x2011;09)
    
    Multiple distinct eu.gcr.io/gardener-project/ci-infra/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/gardener/ci-infra/compare/2e6edca...65a0ac6 | 2023&#x2011;08&#x2011;02&nbsp;&#x2192;&nbsp;2023&#x2011;08&#x2011;09 | ci-infra/golang-test(1.19), ci-infra/golang-test(1.20)
    https://github.com/gardener/ci-infra/compare/a930bab...9cc7d26 | 2023&#x2011;08&#x2011;09&nbsp;&#x2192;&nbsp;2023&#x2011;08&#x2011;09 | ci-infra/branch-cleaner, ci-infra/cherrypicker, ci-infra/cla-assistant, ci-infra/image-builder, ci-infra/job-forker, ci-infra/release-handler

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -26,7 +26,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/mkpj:v20230808-2811823aac \
+  gcr.io/k8s-prow/mkpj:v20230809-b1472a35ec \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
--- hack/check-config.sh
+++ hack/check-config.sh
@@ -20,7 +20,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/checkconfig:v20230808-2811823aac \
+  gcr.io/k8s-prow/checkconfig:v20230809-b1472a35ec \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   --plugin-config=config/prow/plugins.yaml \
