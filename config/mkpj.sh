c21ed2c07195c976caf671cc8f57374e6d101ab9
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2023-08-21 10:36:26 +0200

    Bumping Prow and Prow - ci-infra (#856)
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes/test-infra/compare/916ed2b4b7...9f6571ad1e (2023&#x2011;08&#x2011;18 → 2023&#x2011;08&#x2011;19)
    
    No eu.gcr.io/gardener-project/ci-infra/ changes.

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -26,7 +26,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/mkpj:v20230818-916ed2b4b7 \
+  gcr.io/k8s-prow/mkpj:v20230819-9f6571ad1e \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
--- hack/check-testgrid-config.sh
+++ hack/check-testgrid-config.sh
@@ -20,7 +20,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/configurator:v20230818-916ed2b4b7 \
+  gcr.io/k8s-prow/configurator:v20230819-9f6571ad1e \
   --yaml=config/testgrids/config.yaml \
   --default=config/testgrids/default.yaml \
   --prow-config=config/prow/config.yaml \
