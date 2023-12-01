849cc37ae0a34f058dbde1d54ddefcc40e214c92
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2023-12-01 13:30:45 +0000

    Bumping Prow and Prow - ci-infra
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/4e39ac27ea...2f95ffc454 | 2023&#x2011;11&#x2011;21&nbsp;&#x2192;&nbsp;2023&#x2011;11&#x2011;30 | gcsweb
    https://github.com/kubernetes/test-infra/compare/6bd5ce71de...2f95ffc454 | 2023&#x2011;11&#x2011;27&nbsp;&#x2192;&nbsp;2023&#x2011;11&#x2011;30 | branchprotector, checkconfig, clonerefs, commenter, config-bootstrapper, configurator, crier, deck, entrypoint, generic-autobumper, ghproxy, hook, horologium, initupload, label_sync, mkpj, needs-rebase, prow-controller-manager, sidecar, sinker, status-reconciler, tide
    
    Multiple distinct eu.gcr.io/gardener-project/ci-infra/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/gardener/ci-infra/compare/dc1fff9...8d6c676 | 2023&#x2011;10&#x2011;02&nbsp;&#x2192;&nbsp;2023&#x2011;12&#x2011;01 | ci-infra/copy-images
    https://github.com/gardener/ci-infra/compare/e822f9f...ddd948b | 2023&#x2011;11&#x2011;28&nbsp;&#x2192;&nbsp;2023&#x2011;11&#x2011;30 | ci-infra/golang-test(1.20), ci-infra/golang-test(1.21)
    https://github.com/gardener/ci-infra/compare/e2a62b2...4d2367b | 2023&#x2011;11&#x2011;28&nbsp;&#x2192;&nbsp;2023&#x2011;12&#x2011;01 | ci-infra/branch-cleaner, ci-infra/cherrypicker, ci-infra/cla-assistant, ci-infra/image-builder, ci-infra/job-forker, ci-infra/release-handler

--- hack/check-testgrid-config.sh
+++ hack/check-testgrid-config.sh
@@ -20,7 +20,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/configurator:v20231127-6bd5ce71de \
+  gcr.io/k8s-prow/configurator:v20231130-2f95ffc454 \
   --yaml=config/testgrids/config.yaml \
   --default=config/testgrids/default.yaml \
   --prow-config=config/prow/config.yaml \
