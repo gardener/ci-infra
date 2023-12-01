441840ef55e0a976186edb8e130891a1ef516c3c
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2023-12-01 15:56:12 +0100

    Update Prow to v20231130-2f95ffc454 and Prow - ci-infra as needed (#1025)
    * Bumping Prow and Prow - ci-infra
    
    Multiple distinct gcr.io/k8s-prow/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/kubernetes/test-infra/compare/4e39ac27ea...2f95ffc454 | 2023&#x2011;11&#x2011;21&nbsp;&#x2192;&nbsp;2023&#x2011;11&#x2011;30 | gcsweb
    https://github.com/kubernetes/test-infra/compare/6bd5ce71de...2f95ffc454 | 2023&#x2011;11&#x2011;27&nbsp;&#x2192;&nbsp;2023&#x2011;11&#x2011;30 | branchprotector, checkconfig, clonerefs, commenter, config-bootstrapper, configurator, crier, deck, entrypoint, generic-autobumper, ghproxy, hook, horologium, initupload, label_sync, mkpj, needs-rebase, prow-controller-manager, sidecar, sinker, status-reconciler, tide
    
    Multiple distinct eu.gcr.io/gardener-project/ci-infra/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/gardener/ci-infra/compare/dc1fff9...8d6c676 | 2023&#x2011;10&#x2011;02&nbsp;&#x2192;&nbsp;2023&#x2011;12&#x2011;01 | ci-infra/copy-images
    https://github.com/gardener/ci-infra/compare/e822f9f...ddd948b | 2023&#x2011;11&#x2011;28&nbsp;&#x2192;&nbsp;2023&#x2011;11&#x2011;30 | ci-infra/golang-test(1.20), ci-infra/golang-test(1.21)
    https://github.com/gardener/ci-infra/compare/e2a62b2...4d2367b | 2023&#x2011;11&#x2011;28&nbsp;&#x2192;&nbsp;2023&#x2011;12&#x2011;01 | ci-infra/branch-cleaner, ci-infra/cherrypicker, ci-infra/cla-assistant, ci-infra/image-builder, ci-infra/job-forker, ci-infra/release-handler
    
    * Pin `gcsweb` version
    
    Pin `gcsweb` to last working version `gcr.io/k8s-prow/gcsweb@sha256:d236a4b6a627569a6946800359506ec9393fd254278519d39b73891103cfcf32` that we can enable auto-updates again
    
    ---------
    
    Co-authored-by: Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -26,7 +26,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/mkpj:v20231127-6bd5ce71de \
+  gcr.io/k8s-prow/mkpj:v20231130-2f95ffc454 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
