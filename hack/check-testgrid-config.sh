f888f7caa482d740eb3e752b7d3d2d11c95971a6
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-01-11 00:31:19 +0000

    Bumping Prow and Prow - ci-infra
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes/test-infra/compare/a3102cd4a9...aac7de469e (2024&#x2011;01&#x2011;09 → 2024&#x2011;01&#x2011;10)
    
    Multiple distinct europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/gardener/ci-infra/compare/8fa09bd...0eb9d4c | 2023&#x2011;12&#x2011;23&nbsp;&#x2192;&nbsp;2024&#x2011;01&#x2011;10 | releases/ci-infra/golang-test(1.20), releases/ci-infra/golang-test(1.21)
    https://github.com/gardener/ci-infra/compare/5d342ae...4603974 | 2023&#x2011;12&#x2011;25&nbsp;&#x2192;&nbsp;2024&#x2011;01&#x2011;10 | releases/ci-infra/branch-cleaner, releases/ci-infra/cherrypicker, releases/ci-infra/cla-assistant, releases/ci-infra/image-builder, releases/ci-infra/job-forker, releases/ci-infra/release-handler

--- hack/check-testgrid-config.sh
+++ hack/check-testgrid-config.sh
@@ -20,7 +20,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/configurator:v20240109-a3102cd4a9 \
+  gcr.io/k8s-prow/configurator:v20240110-aac7de469e \
   --yaml=config/testgrids/config.yaml \
   --default=config/testgrids/default.yaml \
   --prow-config=config/prow/config.yaml \
