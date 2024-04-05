0a2d3a19eefc90c390a66bf2451bbef6e19cde71
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-04-05 11:36:42 +0200

    Bumping Prow and Prow - ci-infra (#1441)
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes/test-infra/compare/f1101a8b4e...c76de01869 (2024&#x2011;04&#x2011;03 → 2024&#x2011;04&#x2011;05)
    
    Multiple distinct europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/gardener/ci-infra/compare/8287c21...2432b41 | 2024&#x2011;04&#x2011;04&nbsp;&#x2192;&nbsp;2024&#x2011;04&#x2011;05 | releases/ci-infra/krte(1.21), releases/ci-infra/krte(1.22)
    https://github.com/gardener/ci-infra/compare/8868c0a...2432b41 | 2024&#x2011;04&#x2011;05&nbsp;&#x2192;&nbsp;2024&#x2011;04&#x2011;05 | releases/ci-infra/branch-cleaner, releases/ci-infra/cherrypicker, releases/ci-infra/cla-assistant, releases/ci-infra/image-builder, releases/ci-infra/job-forker, releases/ci-infra/release-handler

--- hack/bootstrap-config.sh
+++ hack/bootstrap-config.sh
@@ -68,7 +68,7 @@ cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
   -v "$temp_kubeconfig":/etc/kubeconfig \
-  gcr.io/k8s-prow/config-bootstrapper:v20240403-f1101a8b4e \
+  gcr.io/k8s-prow/config-bootstrapper:v20240405-c76de01869 \
   --kubeconfig=/etc/kubeconfig \
   --source-path=.  \
   --config-path=config/prow/config.yaml \
--- hack/check-config.sh
+++ hack/check-config.sh
@@ -11,7 +11,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/checkconfig:v20240403-f1101a8b4e \
+  gcr.io/k8s-prow/checkconfig:v20240405-c76de01869 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   --plugin-config=config/prow/plugins.yaml \
