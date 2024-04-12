af6e82ce94bdfe389ac47979137842e888348b09
gardener-ci-robot <gardener.ci.robot@gmail.com>
2024-04-12 08:31:40 +0000

    Update auto-update: renovate

--- config/prow/cluster/renovate/helm/generate-renovate-deployments.sh
+++ config/prow/cluster/renovate/helm/generate-renovate-deployments.sh
@@ -21,6 +21,6 @@ helm repo add renovatebot https://docs.renovatebot.com/helm-charts
 helm repo update
 
 echo "Templating renovate"
-helm template -n renovate renovate renovatebot/renovate --version "37.286.1" -f $SCRIPT_DIR/values.yaml > $SCRIPT_DIR/../renovate_deployment.yaml
+helm template -n renovate renovate renovatebot/renovate --version "37.287.2" -f $SCRIPT_DIR/values.yaml > $SCRIPT_DIR/../renovate_deployment.yaml
 
 echo "Done"
