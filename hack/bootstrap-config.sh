20dcff97619200db7be0efb166688296de4f7097
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-04-18 10:30:39 +0000

    Bumping Prow and Prow - ci-infra
    gcr.io/k8s-prow/ changes: https://github.com/kubernetes-sigs/prow/compare/db89760fe...0a3518154 (2024&#x2011;04&#x2011;17 → 2024&#x2011;04&#x2011;18)
    
    europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes: https://github.com/gardener/ci-infra/compare/01a9db5...c43ded9 (2024&#x2011;04&#x2011;18 → 2024&#x2011;04&#x2011;18)

--- hack/bootstrap-config.sh
+++ hack/bootstrap-config.sh
@@ -68,7 +68,7 @@ cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
   -v "$temp_kubeconfig":/etc/kubeconfig \
-  gcr.io/k8s-prow/config-bootstrapper:v20240417-db89760fe \
+  gcr.io/k8s-prow/config-bootstrapper:v20240418-0a3518154 \
   --kubeconfig=/etc/kubeconfig \
   --source-path=.  \
   --config-path=config/prow/config.yaml \
