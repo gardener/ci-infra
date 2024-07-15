8c110a89f240996446763fa6208b685f4049d6b1
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-07-15 16:50:10 +0200

    Update Helm release renovate to v37.431.6 (#2052)

--- config/prow/cluster/renovate/helm/generate-renovate-deployments.sh
+++ config/prow/cluster/renovate/helm/generate-renovate-deployments.sh
@@ -21,6 +21,6 @@ helm repo add renovatebot https://docs.renovatebot.com/helm-charts
 helm repo update
 
 echo "Templating renovate"
-helm template -n renovate renovate renovatebot/renovate --version "37.431.4" -f $SCRIPT_DIR/values.yaml > $SCRIPT_DIR/../renovate_deployment.yaml
+helm template -n renovate renovate renovatebot/renovate --version "37.431.6" -f $SCRIPT_DIR/values.yaml > $SCRIPT_DIR/../renovate_deployment.yaml
 
 echo "Done"
