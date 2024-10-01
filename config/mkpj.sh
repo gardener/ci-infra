13f5898d83081e7c765a56f2d60a0ca2aae1f214
Oliver Götz <47362717+oliver-goetz@users.noreply.github.com>
2024-10-01 16:46:12 +0200

    Change locations of prow images (#2464)

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -17,7 +17,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/mkpj:v20240805-37a08f946 \
+  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20240930-8302b5c30 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
--- hack/check-testgrid-config.sh
+++ hack/check-testgrid-config.sh
@@ -11,7 +11,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  gcr.io/k8s-prow/configurator:v20240731-a5d9345e59 \
+  gcr.io/k8s-staging-test-infra/configurator:v20240914-93a93a3da9 \
   --yaml=config/testgrids/config.yaml \
   --default=config/testgrids/default.yaml \
   --prow-config=config/prow/config.yaml \
