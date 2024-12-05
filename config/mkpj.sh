3c6c9d7bfec7fc6a312de9e622cd87cb6c8ead85
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2024-12-05 11:30:59 +0000

    Bumping Prow and Prow - test-infra and Prow - ci-infra
    us-docker.pkg.dev/k8s-infra-prow/images/ changes: https://github.com/kubernetes-sigs/prow/compare/786db2030...d4937a693 (2024&#x2011;12&#x2011;04 → 2024&#x2011;12&#x2011;04)
    
    No gcr.io/k8s-staging-test-infra/ changes.
    
    Multiple distinct europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/gardener/ci-infra/compare/fe26335...1f9b883 | 2024&#x2011;12&#x2011;04&nbsp;&#x2192;&nbsp;2024&#x2011;12&#x2011;05 | releases/ci-infra/branch-cleaner, releases/ci-infra/cherrypicker, releases/ci-infra/cla-assistant, releases/ci-infra/image-builder, releases/ci-infra/job-forker, releases/ci-infra/release-handler
    https://github.com/gardener/ci-infra/compare/76102f3...9832e96 | 2024&#x2011;12&#x2011;04&nbsp;&#x2192;&nbsp;2024&#x2011;12&#x2011;05 | releases/ci-infra/golang-test(1.22), releases/ci-infra/golang-test(1.23)

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -17,7 +17,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20241204-786db2030 \
+  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20241204-d4937a693 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
--- hack/bootstrap-config.sh
+++ hack/bootstrap-config.sh
@@ -68,7 +68,7 @@ cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
   -v "$temp_kubeconfig":/etc/kubeconfig \
-  us-docker.pkg.dev/k8s-infra-prow/images/config-bootstrapper:v20241204-786db2030 \
+  us-docker.pkg.dev/k8s-infra-prow/images/config-bootstrapper:v20241204-d4937a693 \
   --kubeconfig=/etc/kubeconfig \
   --source-path=.  \
   --config-path=config/prow/config.yaml \
