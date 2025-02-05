d66074355abee7941e0c6f8eec2524b9b5b8c69d
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-02-05 12:20:30 +0100

    Bumping Prow and Prow - test-infra and Prow - ci-infra (#3141)
    us-docker.pkg.dev/k8s-infra-prow/images/ changes: https://github.com/kubernetes-sigs/prow/compare/d572f855b...e871edfd1 (2025&#x2011;01&#x2011;24 → 2025&#x2011;02&#x2011;05)
    
    No gcr.io/k8s-staging-test-infra/ changes.
    
    Multiple distinct europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes:
    
    Commits | Dates | Images
    --- | --- | ---
    https://github.com/gardener/ci-infra/compare/cb1d1c4...1ea5c2c | 2025&#x2011;01&#x2011;31&nbsp;&#x2192;&nbsp;2025&#x2011;02&#x2011;05 | releases/ci-infra/golang-test(1.22), releases/ci-infra/golang-test(1.23)
    https://github.com/gardener/ci-infra/compare/066e5bc...9e64cf3 | 2025&#x2011;02&#x2011;03&nbsp;&#x2192;&nbsp;2025&#x2011;02&#x2011;05 | releases/ci-infra/branch-cleaner, releases/ci-infra/cherrypicker, releases/ci-infra/cla-assistant, releases/ci-infra/image-builder, releases/ci-infra/job-forker, releases/ci-infra/release-handler

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -17,7 +17,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250124-d572f855b \
+  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250205-e871edfd1 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
