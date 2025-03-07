875cc1f2e5da23de7140e3d5abec079b748086fd
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-03-07 16:36:44 +0100

    Bumping Prow and Prow - test-infra and Prow - ci-infra (#3345)
    us-docker.pkg.dev/k8s-infra-prow/images/ changes: https://github.com/kubernetes-sigs/prow/compare/0e909e33e...7a867cddc (2025&#x2011;03&#x2011;04 → 2025&#x2011;03&#x2011;07)
    
    No gcr.io/k8s-staging-test-infra/ changes.
    
    No europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes.

--- hack/check-config.sh
+++ hack/check-config.sh
@@ -11,7 +11,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  us-docker.pkg.dev/k8s-infra-prow/images/checkconfig:v20250304-0e909e33e \
+  us-docker.pkg.dev/k8s-infra-prow/images/checkconfig:v20250307-7a867cddc \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   --plugin-config=config/prow/plugins.yaml \
