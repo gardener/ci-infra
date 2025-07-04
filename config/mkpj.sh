800f6851627efd588080f05ab0aa3f2a074e030f
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-07-04 00:30:59 +0000

    Bumping Prow and Prow - test-infra and Prow - ci-infra
    us-docker.pkg.dev/k8s-infra-prow/images/ changes: https://github.com/kubernetes-sigs/prow/compare/3e50f2237...9d994e8dc (2025&#x2011;06&#x2011;26 → 2025&#x2011;07&#x2011;04)
    
    No gcr.io/k8s-staging-test-infra/ changes.
    
    europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes: https://github.com/gardener/ci-infra/compare/322c0a7...4643fec (2025&#x2011;07&#x2011;03 → 2025&#x2011;07&#x2011;03)

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -17,7 +17,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250626-3e50f2237 \
+  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250704-9d994e8dc \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
--- hack/check-config.sh
+++ hack/check-config.sh
@@ -11,7 +11,7 @@ set -o pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  us-docker.pkg.dev/k8s-infra-prow/images/checkconfig:v20250626-3e50f2237 \
+  us-docker.pkg.dev/k8s-infra-prow/images/checkconfig:v20250704-9d994e8dc \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   --plugin-config=config/prow/plugins.yaml \
