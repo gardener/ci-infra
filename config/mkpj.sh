fc2a5684cfc1892175482419ddb28ecfcbc56610
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2025-09-02 12:30:44 +0000

    Bumping Prow and Prow - test-infra and Prow - ci-infra
    us-docker.pkg.dev/k8s-infra-prow/images/ changes: https://github.com/kubernetes-sigs/prow/compare/25d79acb8...ccceec7a2 (2025&#x2011;08&#x2011;23 → 2025&#x2011;09&#x2011;02)
    
    No gcr.io/k8s-staging-test-infra/ changes.
    
    europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes: https://github.com/gardener/ci-infra/compare/410dfc2...4a29185 (2025&#x2011;09&#x2011;02 → 2025&#x2011;09&#x2011;02)

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -17,7 +17,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250823-25d79acb8 \
+  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20250902-ccceec7a2 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
--- hack/bootstrap-config.sh
+++ hack/bootstrap-config.sh
@@ -68,7 +68,7 @@ cd "$(git rev-parse --show-toplevel)"
 
 docker run --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
   -v "$temp_kubeconfig":/etc/kubeconfig \
-  us-docker.pkg.dev/k8s-infra-prow/images/config-bootstrapper:v20250823-25d79acb8 \
+  us-docker.pkg.dev/k8s-infra-prow/images/config-bootstrapper:v20250902-ccceec7a2 \
   --kubeconfig=/etc/kubeconfig \
   --source-path=.  \
   --config-path=config/prow/config.yaml \
