a768d51f317b715a029f6089d5c750d0edb2155e
vpnachev <vladimir.nachev@sap.com>
2025-09-16 11:14:33 +0300

    Enhance copy-images to overwrite destination tags

--- config/images/copy-images.sh
+++ config/images/copy-images.sh
@@ -10,12 +10,14 @@ if [ -z "$filepath" ] ; then
 fi
 
 # Loop over the images in the YAML
-for source in $(yq '.images[].source' "$filepath"); do
-    image=$(yq '.images[] | select(.source == "'"$source"'")' "$filepath")
-    destination=$(echo "$image" | yq '.destination')
+for image in $(yq '.images[] | @json' "$filepath"); do
+    source=$(yq '.source' <<< $image)
+    destination=$(yq '.destination' <<< $image)
     # Loop over the tags for the current image
-    for tag in $(echo "$image" | yq '.tags[]'); do
+    for tag in $(yq '.tags[] | @json' <<< $image ); do
         # Copy the container image for the current tag using crane
-        crane copy "$source:$tag" "$destination:$tag"
+        source_tag=$(yq '.source'<<< $tag)
+        destination_tag=$(yq '.destination // .source'<<< $tag)
+        crane copy "$source:$source_tag" "$destination:$destination_tag"
     done
 done
