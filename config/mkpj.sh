9318c11c43724c722c66d94d6d1fe114848f6587
Gardener Prow Robot <gardener.ci.robot@gmail.com>
2026-03-10 09:37:06 +0100

    Bumping Prow components - prow and Prow components - test-infra and Prow components - ci-infra (#5648)
    us-docker.pkg.dev/k8s-infra-prow/images/ changes: https://github.com/kubernetes-sigs/prow/compare/c6334a44a...df7422152 (2026&#x2011;03&#x2011;09 → 2026&#x2011;03&#x2011;10)
    
    No gcr.io/k8s-staging-test-infra/ changes.
    
    No europe-docker.pkg.dev/gardener-project/releases/ci-infra/ changes.

--- config/mkpj.sh
+++ config/mkpj.sh
@@ -17,7 +17,7 @@
 cd "$(git rev-parse --show-toplevel)"
 
 docker run -i --rm -w /etc/ci-infra -v $PWD:/etc/ci-infra \
-  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20260309-c6334a44a \
+  us-docker.pkg.dev/k8s-infra-prow/images/mkpj:v20260310-df7422152 \
   --config-path=config/prow/config.yaml \
   --job-config-path=config/jobs \
   "$@"
